ace43c51777e625219e9e9637febbb1b82a4395b
magastonbell <maximilien.gaston-bellegarde@etu.u-bordeaux.fr>
2025-12-22 14:08:52 +0100

    free added

---
+++ free.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "struct.h"
+
+void free_matrix(matrix *m)
+{
+    for (int i = 0; i < m->dimension; i++)
+    {
+        free(m->mat[i]);
+    }
+    free(m->mat);
+    return;
+}
---
+++ free.h
@@ -0,0 +1,4 @@
+#include <stdio.h>
+#include "struct.h"
+
+void free_matrix(matrix *m);
